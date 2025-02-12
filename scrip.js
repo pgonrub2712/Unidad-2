9d511188bbc85700a86ead261af3347b271ecd18
pgonrub2712 <pgonrub2712@g.educaand.es>
2025-02-12 19:25:49 +0100

    Add files via upload

---
+++ scrip.js
@@ -0,0 +1,11 @@
+    function saludar(){
+        alert("Hola churri");
+        emoji();
+    }
+    function Despedirse() {
+        alert("Adios churri")
+        emoji();
+    }
+    function emoji() {
+        alert(":)")
+    }
