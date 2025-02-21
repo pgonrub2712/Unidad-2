f331f9828c997a925f40eaf0d41e8bbb6ab558e2
pgonrub2712 <pgonrub2712@g.educaand.es>
2025-02-21 11:19:08 +0100

    Add files via upload

---
+++ Actividad 4/js/script.js
@@ -0,0 +1,5 @@
+function calcular() {
+    let a = parseInt    (prompt ("dice el primer número"),10);
+    let b = parseInt    (prompt ("dice el segundo número"), 10);
+    alert ("El primer número es; " + a + "\nEl segundo número es; " + b +"\nLa suma es; " + (a+b) + "\nLa resta es; " + (a-b) + "\nLa multiplicación es:  " + (a*b) + "\nLA division es: " + (a/b) + "\nEl resto es: " + (a&b) + "");
+}
