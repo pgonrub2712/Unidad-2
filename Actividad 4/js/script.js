90b00b6c625d785b7f53222ef715a1b2da6d2ffc
pgonrub2712 <pgonrub2712@g.educaand.es>
2025-02-21 11:18:50 +0100

    Delete Actividad 4 directory

--- Actividad 4/js/script.js
+++
@@ -1,5 +0,0 @@
-function calcular() {
-    let a = parseInt    (prompt ("dice el primer número"),10);
-    let b = parseInt    (prompt ("dice el segundo número"), 10);
-    alert ("El primer número es; " + a + "\nEl segundo número es; " + b +"\nLa suma es: " + (a+b) +"\nLa resta es. + (a-b) +"\nLa multiplicacion es: " + (a*b) + "\nLA division es: " + (a/b) + "\nEl resto es: " + (a%) + ");
-}
